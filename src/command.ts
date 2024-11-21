3e66beb05f716d0aa99501d57c22613613ccb466
WaterWolf5918 <87424826+WaterWolf5918@users.noreply.github.com>
2024-11-20 20:25:58 -0500

    Updated Stuff

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,6 @@
-import { SlashCommandBuilder, CommandInteraction, CacheType, SlashCommandSubcommandsOnlyBuilder, SlashCommandSubcommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, CommandInteraction, CacheType, SlashCommandSubcommandsOnlyBuilder, SlashCommandSubcommandBuilder, SlashCommandOptionsOnlyBuilder } from 'discord.js';
 
 
 
-export type Command = { commandBuilder: Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup' > | SlashCommandSubcommandsOnlyBuilder, runnable: CommandRunnable};
+export type Command = { commandBuilder: Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup' > | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder, runnable: CommandRunnable};
 export type CommandRunnable = (interaction: CommandInteraction<CacheType>) => void;
